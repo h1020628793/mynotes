22a1aa1f06ff41d2a731b650a3789ab6921292ef
TomChen <76876201@qq.com>
2020-11-06 15:37:11 +0800

    up

--- 04-project/blog/routes/index.js
+++ 04-project/blog/routes/index.js
@@ -2,10 +2,36 @@
 const express = require('express')
 const router = express.Router()
 
+const Category = require('../models/category')
+
+//获取共通数据
+const getCommonData = async ()=>{
+    const categoriesPromise = Category.find({}, "name")
+    const categories = await categoriesPromise
+    return {
+        categories
+    }
+}
+
+
+
 //显示首页
-router.get("/", (req, res) => {
+router.get("/", async (req, res) => {
+    //获取分类
+    const {categories} = await getCommonData()
     res.render('main/index', {
-        userInfo:req.userInfo
+        userInfo:req.userInfo,
+        categories
+    })
+})
+//显示列表页面
+router.get("/list/:id", async (req, res) => {
+    const { id } = req.params
+    const { categories } = await getCommonData()
+    res.render('main/list', {
+        userInfo: req.userInfo,
+        categories,
+        currentCategory:id
     })
 })
 
